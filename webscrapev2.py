881aeb7ea1aeb22af84e70a9dda3fa1a0b038ef1
unknown <johnramilreyes@gmail.com>
2021-10-19 21:39:53 +0800

    Ramil 9:39

--- webscrapev2.py
+++ webscrapev2.py
@@ -12,7 +12,7 @@ products= []
 
 
 
-shopee = "https://shopee.ph/Ceramic-Matte-Full-Screen-Film-For-REALME-3-5-5i-5PRO-6-6i-6PRO-7-7i-C2-C3-C11-C15-C17-i.324268637.7761207154?position=32"
+shopee = "https://shopee.ph/Face-mask-holder-Lanyard-for-face-mask-strap-chain-necklace-Pearl-Eyeglasses-Lanyards-Neck-Hanging-R-i.423714846.9664845009?sp_atk=c6a4b0cc-5df0-4fdc-8fe3-4e26169042d8"
 
 rating = []
 
@@ -22,20 +22,12 @@ CO.add_argument('--ignore-certificate-errors')
 CO.add_argument('--start-maximized')
 driver = webdriver.Firefox(executable_path=r'C:\Program Files (x86)\geckodriver.exe',options=CO)
 driver.get(shopee)
-driver.implicitly_wait(5)
+driver.implicitly_wait(7)
 
 i = 1
 
-title = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[2]/div[2]/div[2]/div[3]/div/div[1]/span')
-print(title.text)
-prod_title = {'Product_Name': title.text}
-products.append(prod_title)
-
-comment = driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[2]/div[2]/div[3]/div[3]/div[1]/div[2]/div/div[2]/div[2]/div[7]")
-
-comment.click();
 while True:
-    solid_button = driver.find_element_by_class_name('shopee-button-solid')
+    solid_button = driver.find_element_by_class_name('shopee-button-solid--primary ')
     if int(solid_button.text)== 1:
         driver.execute_script('window.scrollTo(0,document.body.scrollHeight);')
         time.sleep(4)
@@ -50,14 +42,14 @@ while True:
     if int(solid_button.text)!= i:
         break;
         
-    right_button = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[2]/div[2]/div[3]/div[3]/div[1]/div[2]/div/div[3]/div[2]/button[8]')
+    right_button = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[2]/div/div[3]/div[3]/div[1]/div[2]/div/div[3]/div[2]/button[8]')
     if right_button:
         while True: 
                 i = i+1
                 right_button.click();
-                solid_button = driver.find_element_by_class_name('shopee-button-solid')
+                solid_button = driver.find_element_by_class_name('shopee-button-solid--primary ')
                 if int(solid_button.text)== i:
-                    solid_button = driver.find_element_by_class_name('shopee-button-solid')
+                    solid_button = driver.find_element_by_class_name('shopee-button-solid--primary ')
                     driver.execute_script('window.scrollTo(0,4000);')                
                     time.sleep(3)
                     prod_reviews = driver.find_elements_by_class_name('shopee-product-rating__content')
@@ -65,13 +57,13 @@ while True:
                         print(prod_review.text.encode('unicode_escape').decode('utf-8'))
                         product = {'Product_Reviews': prod_review.text}
                         products.append(product)
-                       
-                if int(solid_button.text)!= i:
+
+                if int(solid_button.text) != i:
                     break;
     
     break;
 df = pd.DataFrame(products, columns = ['Product_Name','Product_Reviews'])
             
-df.to_csv('C:/Users/HP/Documents/Thesis Coding/Web Scrapper Phyton/prodreview2.csv',columns=['Product_Name','Product_Reviews'],index = False)
+df.to_csv('C:/Users/johnr/Documents/Thesis Coding/Webscraper/jeremy.csv',columns=['Product_Reviews'],index = False, encoding = 'utf-8')
 
 driver.close()
