be6b043dab38fd33e7c2c9a8089faf64a02cc7c2
Renzel <renzelywjc@gmail.com>
2021-10-21 21:04:42 +0800

    updated 9:04 pm

--- Interface/js/main.js
+++ Interface/js/main.js
@@ -1,8 +1,14 @@
+$(document).ready(function(){
+  $("#sentimentArea").emojioneArea();
+})
+
 $('.interactive-menu-button a').click(function() {
   $(this).toggleClass('active');
 });
 
 var scroll = new SmoothScroll('a[href*="#"]');
+
+
 
 
 $('.more-btn').click(function() {
@@ -14,8 +20,6 @@ $('.more-btn').click(function() {
     $(".more-btn-inside").text("Show More.");
   }
 });
-
-
 
 
 
