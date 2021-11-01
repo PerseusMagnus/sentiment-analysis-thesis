40003d27b7506c8638c4a33ce1a1800f9e24f0da
Gil Jeremy Ditbalan <ditablangiljeremy@gmail.com>
2021-11-01 22:44:32 +0800

    updated 10:44 pm jeremy

--- Interface/app.py
+++ Interface/app.py
@@ -8,7 +8,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return  render_template("index.html")
+    return render_template("index.html")
+
+@app.route('/sentiment',methods=['POST','GET'])
+def sentiment():
+    return render_template("analyze.html")
     
 
 @app.route('/predict',methods=['POST','GET'])
@@ -21,17 +25,25 @@ def predict():
         # getting input with name = fname in HTML form
         sentiment = model.predict_sentiment(input)
     else:
-        return render_template("index.html",sentiment='Predicted Sentiment:  Positive')
+        return render_template("analyze.html",sentiment='Input is Empty')
     
     if(sentiment==2):
-        return render_template("index.html",sentiment='Predicted Sentiment:  Positive')
+        return render_template("analyze.html",sentiment='Predicted Sentiment:  Positive')
 
     if(sentiment==1):
-        return render_template("index.html",sentiment='Predicted Sentiment:  Neutral')
+        return render_template("analyze.html",sentiment='Predicted Sentiment:  Neutral')
 
     if(sentiment==0):
-        return render_template("index.html",sentiment='Predicted Sentiment:  Negative')
+        return render_template("analyze.html",sentiment='Predicted Sentiment:  Negative')
     
+@app.route('/predict_multiple',methods=['POST','GET'])
+
+def predict_multiple():
+    
+    input = request.form.get("sentimentArea")
+
+    return render_template("analyze.html",sentiment='Predicted Sentiment:  Negative')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- Interface/tempCodeRunnerFile.py
+++ Interface/tempCodeRunnerFile.py
@@ -1,1 +1,1 @@
-predict
+Positive
