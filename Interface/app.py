16c85cd301865f10ba76aab54fffbe97ded8ae66
johnramil02 <johnramilreyes@gmail.com>
2021-11-15 02:54:30 +0800

    Ramil 2:54 pm

--- Interface/app.py
+++ Interface/app.py
@@ -21,7 +21,7 @@ input_with_polarity = []
 app.config["DEBUG"] = True
 
 # Upload folder for files
-UPLOAD_FOLDER = 'C:/Users/ditab/Documents/thesis development/sentiment-analysis-thesis/Interface/static/files/'
+UPLOAD_FOLDER = 'C:/Users/johnr/Documents/Sentiment Analysis/sentiment-analysis-thesis/Interface/static/files/'
 app.config['UPLOAD_FOLDER'] =  UPLOAD_FOLDER
 
 
